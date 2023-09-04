b37dfd8cf4b36316145d9f032bd769c047fd2abf
Shanto57575 <shanto57575@gmail.com>
2023-09-04 23:52:48 +0600

    vercel deployment

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function run() {
     try {
         await client.connect();
 
-        const airbnbCollection = client.db("airbnb").collection("airbnbInfo");
+        const airbnbCollection = client.db("airbnbDB").collection("airbnbInfo");
 
         app.get('/categories', async (req, res) => {
             const cursor = airbnbCollection.find();
@@ -32,7 +32,7 @@ async function run() {
         })
 
         app.get('/categories/:category', async (req, res) => {
-            const category = req.params.category;
+            const category = req.params?.category;
 
             let query = {};
 
@@ -59,9 +59,86 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/categories/:continent', async (req, res) => {
+            const continent = req.params?.continent;
+
+            let query = {};
+
+            if (continent === undefined) {
+                return;
+            }
+            else {
+                query = { continent: continent }
+            }
 
 
-        // Send a ping to confirm a successful connection
+            const options = {
+                projection: {
+                    category: 1,
+                    hostName: 1,
+                    img: 1,
+                    destination: 1,
+                    price: {
+                        $toDouble: "$price"
+                    },
+                    time: 1,
+                    rating: 1,
+                    continent: 1
+                }
+            }
+
+
+            const result = await airbnbCollection.find(query, options).toArray();
+            res.send(result);
+        })
+
+        app.get('/filter', async (req, res) => {
+            const { type, priceRange, bedrooms, beds, bathrooms, propertyType } = req.query;
+            console.log(type, priceRange, bedrooms, beds, bathrooms, propertyType);
+            let filter = {};
+
+            if (type && type !== 'any') {
+                filter.type = type;
+            }
+
+            if (priceRange) {
+                const [minPrice, maxPrice] = priceRange.split('-');
+                filter.price = {
+                    $gte: parseInt(minPrice),
+                    $lte: parseInt(maxPrice)
+                };
+            }
+
+            if (bedrooms && bedrooms !== 'any') {
+                filter.bedrooms = parseInt(bedrooms);
+            }
+
+            if (beds && beds !== 'any') {
+                filter.beds = parseInt(beds);
+            }
+
+            if (bathrooms && bathrooms !== 'any') {
+                filter.bathrooms = parseInt(bathrooms);
+            }
+
+            if (propertyType && propertyType !== 'any') {
+                filter.propertyType = propertyType;
+            }
+
+            const result = await airbnbCollection.find(filter).toArray();
+            const count = result.length;
+            console.log("count", count);
+
+            if (result.length === 0) {
+                res.send({ count: 0, matchingData: result });
+            }
+            else {
+                res.send({ count: result.length, matchingData: result });
+            }
+
+            console.log(result);
+        })
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
